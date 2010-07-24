363dd4bb5d0ac6c1b1b618d94f862f92bec2fcb5
Rico Sta. Cruz <rico@ambiescent.com>
2010-07-25 02:26:18 +0800

    Update the datamapper-sqlite recipe to rely on the datamapper recipe.

--- recipes/datamapper-sqlite.rb
+++ recipes/datamapper-sqlite.rb
@@ -1,9 +1,7 @@
-gem_install 'datamapper'
+install_package 'datamapper'
+
 gem_install 'sqlite3-ruby'
 gem_install 'dm-sqlite-adapter'
-
-add_require 'dm-core'
-add_require 'dm-validations'
 add_require 'dm-sqlite-adapter'
 
 create_file 'config/datamapper.example.yml', I(%{
@@ -24,28 +22,10 @@ create_file 'config/datamapper.example.yml', I(%{
   :production:
     <<: *defaults
     :database: db/sqlite/production
-})
+}), :force => true
 
 empty_directory 'db/sqlite/test'
 empty_directory 'db/sqlite/production'
 
-create_file 'lib/thors/datamapper.thor', I(%{
-  class Monk < Thor
-    add_config_file 'config/datamapper.example.yml'
-  end
-})
-
-add_initializer I(%{
-  DataMapper.setup(:default, app_config(:datamapper))
-})
-
-add_class_def I(%{
-  configure :development do
-    DataMapper::Logger.new(STDOUT, :debug)
-  end
-})
-
 append_file '.gitignore', I(%{
-  /config/datamapper.yml
-  /db/sqlite
 })
